e383d02fd2e317fdcd653819ecea41dd58d94eee
Yemelyanov <nikolaef.post@gmail.com>
2023-03-31 15:42:00 +0300

    feat: pre deploy  edit path 1

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ export const router = createBrowserRouter([
         element: <Layout  />,
         children: [
             {
-                path: "fashion-store",
+                path: "/fashion-store",
                 element: <Home  />,
             },
             {
