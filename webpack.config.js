c822613ca81c9f4c5177fb2175854cb52ae34ec3
Yemelyanov <nikolaef.post@gmail.com>
2023-03-31 20:34:01 +0300

    feat: pre deploy  all back

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,8 @@ let conf = {
     output: {
         path: path.resolve(__dirname, './docs'),
         filename: 'main.js',
-        publicPath: '',
-        assetModuleFilename: "docs/assets/[name][ext]"
+        publicPath: '/docs/',
+        assetModuleFilename: "assets/[name][ext]"
     },
     devServer: {
         static: {
