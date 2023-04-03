f760ed97207affa8a0d51323a04dd67639ab5831
Yemelyanov <nikolaef.post@gmail.com>
2023-04-03 13:53:47 +0300

    feat: pre deploy  all back

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, createHashRouter} from "react-router-dom";
 import {Catalog, Order, Layout, Error404, Home, Product, PersonalArea} from "./pages";
 
 
 
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
     {
         // path: "",
         element: <Layout  />,
